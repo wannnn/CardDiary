d00b8667356b41c9ac852e5461daacfabeb4b4d8
Claire <wanru850111@gmail.com>
2020-08-17 18:19:01 +0800

    ui 微調

--- app/src/main/java/com/claire/carddiary/edit/EditFragment.kt
+++ app/src/main/java/com/claire/carddiary/edit/EditFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.claire.carddiary.databinding.FragEditBinding
 import com.google.android.material.tabs.TabLayoutMediator
@@ -24,11 +25,14 @@ class EditFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val adapter = ImagePagerAdapter(List(3) {""})
+        val adapter = ImagePagerAdapter(List(3) {""}).apply {
+            listener = {
+                Toast.makeText(context, "open gallery", Toast.LENGTH_SHORT).show()
+            }
+        }
         binding.imagePager.adapter = adapter
 
-        TabLayoutMediator(binding.indicator, binding.imagePager)
-        { tab, position -> }.attach()
+        TabLayoutMediator(binding.indicator, binding.imagePager) { _, _ -> }.attach()
 
     }
 }
